c4364740d0e293af3e607fee7203d8fb6a2e44ff
Geuniv <kkoowl99@gmail.com>
2023-03-29 15:34:44 +0900

    강의 예제 추가 및 수정

---
+++ src/main/java/Ex05/Ex05_03.java
@@ -0,0 +1,17 @@
+package Ex05;
+
+public class Ex05_03 {
+
+    public static void main(String[] args) {
+
+        int a = 200;
+
+        if ( a < 100 ) {
+            // 5행이 참이면 중괄호로 묶인 부분이 모두 수행된다.
+            System.out.printf("100보다 작군요.. \n");
+            System.out.printf("거짓이므로 앞의 문장은 안보이겠죠 ? \n");
+        }
+
+        System.out.printf("프로그램 끝 ! \n");
+    }
+}
