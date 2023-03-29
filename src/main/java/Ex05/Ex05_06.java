cbc1c6122e83dad1fe814bf8fbee7cb1c065eb4c
Geuniv <kkoowl99@gmail.com>
2023-03-29 15:35:36 +0900

    강의 예제 추가 및 수정

--- src/main/java/Ex05/Ex05_06.java
+++ src/main/java/Ex05/Ex05_06.java
@@ -13,12 +13,12 @@ public class Ex05_06 {
         int a;
 
         System.out.printf("정수를 입력하세요 : ");
-        a = s.nextInt();
+        a = s.nextInt(); // 정수를 입력받는다.
         
-        if (a % 2 == 0){
-            System.out.printf("짝수를 입력했군요.. \n");
+        if (a % 2 == 0){ // 입력한 값을 2로 나누어 나머지가 0이면 ( 즉 짝수이면 )
+            System.out.printf("짝수를 입력했군요.. \n"); // 입력한 값이 짝수이면 실행한다.
         } else {
-            System.out.printf("홀수를 입력했군요.. \n");
+            System.out.printf("홀수를 입력했군요.. \n"); // 입력한 값이 짝수가 아니면 실행한다.
         }
 
     }
