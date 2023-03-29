f3fb9c9fb1ec332651659969c820576599b03738
Geuniv <kkoowl99@gmail.com>
2023-03-29 15:52:39 +0900

    강의 예제 추가 및 수정

---
+++ src/main/java/Ex06/Ex06_01.java
@@ -0,0 +1,13 @@
+package Ex06;
+
+public class Ex06_01 {
+
+    public static void main(String[] args) {
+        // 내용을 출력한다.
+        System.out.printf("안녕하세요 ? 빙글빙글 for 문을 공부중입니다 ^^ \n");
+        System.out.printf("안녕하세요 ? 빙글빙글 for 문을 공부중입니다 ^^ \n");
+        System.out.printf("안녕하세요 ? 빙글빙글 for 문을 공부중입니다 ^^ \n");
+        System.out.printf("안녕하세요 ? 빙글빙글 for 문을 공부중입니다 ^^ \n");
+        System.out.printf("안녕하세요 ? 빙글빙글 for 문을 공부중입니다 ^^ \n");
+    }
+}
