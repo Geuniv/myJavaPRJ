94b9d910c8324636990cd5841b95ebf70e29b663
Geuniv <kkoowl99@gmail.com>
2023-03-29 15:34:33 +0900

    강의 예제 추가 및 수정

---
+++ src/main/java/Ex05/Ex05_01.java
@@ -0,0 +1,12 @@
+package Ex05;
+
+public class Ex05_01 {
+
+    public static void main(String[] args) {
+
+        int a = 99;
+
+        if ( a < 100 ) // a가 100보다 작으므로 참이다.
+            System.out.printf("100보다 작군요.. \n");
+    }
+}
