717fa2cbaf28aee947f6f352c87713d0a763dc22
Geuniv <kkoowl99@gmail.com>
2023-03-29 15:35:27 +0900

    강의 예제 추가 및 수정

--- src/main/java/Ex05/Ex05_05.java
+++ src/main/java/Ex05/Ex05_05.java
@@ -7,9 +7,11 @@ public class Ex05_05 {
         int a = 200;
 
         if (a < 100){
+            // 5행이 참이면 ( a가 100보다 작으면 ) 실행한다.
             System.out.printf("100보다 작군요...\n");
             System.out.printf("참이면 이 문장도 보이겠죠 ? \n");
         } else {
+            // 5행이 거짓이면 ( a가 100보다 크거나 같으면 ) 실행한다.
             System.out.printf("100보다 크군요.. \n");
             System.out.printf("거짓이면 이 문장도 보이겠죠 ? \n");
         }
