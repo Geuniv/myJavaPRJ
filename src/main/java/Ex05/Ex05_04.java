be68ac66d54eb25ad7f4de765a4a8d0b18ad7cca
Geuniv <kkoowl99@gmail.com>
2023-03-29 15:35:15 +0900

    강의 예제 추가 및 수정

---
+++ src/main/java/Ex05/Ex05_04.java
@@ -0,0 +1,15 @@
+package Ex05;
+
+public class Ex05_04 {
+    public static void main(String[] args) {
+
+        int a = 200;
+
+        if ( a < 100)
+            // 5행이 참이면 ( a가 100보다 작으면 ) 실행한다.
+            System.out.printf("100보다 작군요.. \n");
+        else
+            // 5행이 거짓이면 ( a가 100보다 크거나 같으면 ) 실행한다.
+            System.out.printf("100보다 크군요.. \n");
+    }
+}
