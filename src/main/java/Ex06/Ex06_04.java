cfe01fe36332e407f1c82817a32ac315d596f9d0
Geuniv <kkoowl99@gmail.com>
2023-04-05 17:53:33 +0900

    강의 예제 추가 및 수정 ( 6장 )

---
+++ src/main/java/Ex06/Ex06_04.java
@@ -0,0 +1,14 @@
+package Ex06;
+
+public class Ex06_04 {
+
+    public static void main(String[] args) {
+
+        int i;
+
+        // 초깃값, 조건식, 증감식을 수정하여 5에서 1씩 줄여간다.
+        for ( i = 5; i > 0; i-- ) {
+            System.out.printf("%d : 안녕하세요 ? 빙글빙글 for 문을 공부중입니다. ^^7 \n", i);
+        }
+    }
+}
