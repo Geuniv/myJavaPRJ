7bc42de77f1ef7d0de99034260b1dbd5a591852c
Geuniv <kkoowl99@gmail.com>
2023-04-05 17:53:49 +0900

    강의 예제 추가 및 수정 ( 6장 )

---
+++ src/main/java/Ex06/Ex06_06.java
@@ -0,0 +1,14 @@
+package Ex06;
+
+public class Ex06_06 {
+
+    public static void main(String[] args) {
+
+        int hap;
+
+        // for 문을 사용하지 않고 1 부터 10까지의 값을 더해 입력한다.
+        hap = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+
+        System.out.printf("1에서 10까지의 합 : %d \n", hap);
+    }
+}
