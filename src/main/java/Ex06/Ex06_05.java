a744686e9d3e062c171b00b0974c37fca6236c92
Geuniv <kkoowl99@gmail.com>
2023-04-05 17:53:41 +0900

    강의 예제 추가 및 수정 ( 6장 )

---
+++ src/main/java/Ex06/Ex06_05.java
@@ -0,0 +1,14 @@
+package Ex06;
+
+public class Ex06_05 {
+
+    public static void main(String[] args) {
+
+        int i;
+
+        // i 값이 1부터 5까지 변경된다.
+        for ( i = 1; i <= 5; i++ ) {
+            System.out.printf("%d \n", i);
+        }
+    }
+}
