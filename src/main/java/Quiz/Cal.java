3424587700b833313d4cbc121e6a7043fa09dfa2
Geuniv <kkoowl99@gmail.com>
2023-05-17 09:27:24 +0900

    2023.05.17 잔디 심기 ( 자바 아침 문제 ( 상속 ) )

---
+++ src/main/java/Quiz/Cal.java
@@ -0,0 +1,10 @@
+package Quiz;
+
+/* ( 부모클래스 ) 기본 규약중 하나는 자바 파일 하나 당 무조건 클래스 하나씩만 사용. */
+/* 상속은 무조건 객체 ( Class ) 끼리만 가능함 함수를 상속한다 ? 뭐 이런 문법은 없다. */
+public class Cal {
+
+    public int sum (int a, int b) {
+        return a + b;
+    }
+}
