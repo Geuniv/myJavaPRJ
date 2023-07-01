366741e5df1766d175cd664b964fab9b2a960f6e
Geuniv <kkoowl99@gmail.com>
2023-07-01 23:24:18 +0900

    2023.07.01 잔디 심기 ( 자바 예제 )

---
+++ src/main/java/Quiz/Quiz6.java
@@ -0,0 +1,13 @@
+package Quiz;
+
+public class Quiz6 {
+
+    public static void main(String[] args) {
+
+        int i, j;
+
+        for ( i = 0, j = 1; i < 6; j++ )
+            System.out.printf("*");
+
+    }
+}
