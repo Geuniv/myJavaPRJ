3e9eb3be74b367d16454d7c54fd8332cc2213bf8
Geuniv <kkoowl99@gmail.com>
2025-02-20 23:52:47 +0900

    2025.02.20

---
+++ src/main/java/Programmers/Lv0/Programmers_181952.java
@@ -0,0 +1,13 @@
+package Programmers.Lv0;
+
+import java.util.Scanner;
+
+public class Programmers_181952 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.print(a);
+    }
+}
