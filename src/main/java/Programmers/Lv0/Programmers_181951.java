2737dd27267b1fdf2dee610b0087c2e9e116acee
Geuniv <kkoowl99@gmail.com>
2025-02-27 21:42:18 +0900

    2025.02.27

---
+++ src/main/java/Programmers/Lv0/Programmers_181951.java
@@ -0,0 +1,16 @@
+package Programmers.Lv0;
+
+import java.util.Scanner;
+
+public class Programmers_181951 {
+
+    // 코딩 기초 트레이닝 - a와 b 출력하기
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("a = %d\n", a);
+        System.out.printf("b = %d", b);
+    }
+}
--- src/main/java/Programmers/Lv0/Programmers_181952.java
+++ src/main/java/Programmers/Lv0/Programmers_181952.java
@@ -3,6 +3,8 @@ package Programmers.Lv0;
 import java.util.Scanner;
 
 public class Programmers_181952 {
+
+    // 코딩 기초 트레이닝 - 문자열 출력하기
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
